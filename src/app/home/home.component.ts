ee14e707fd9c25feb52d9b9b2cbd93d97d50a11a
YuChih Hu <hijac.hu@gmail.com>
2020-02-15 13:35:57 +0800

    apply environment variable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from './../services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'home',
@@ -8,6 +9,7 @@ import { AuthService } from './../services/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  homeColor = environment.homeColor;
 
   constructor(public authService: AuthService) { }
 }
